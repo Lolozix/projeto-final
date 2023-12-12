cd3c5226caa5458239c336b10d517138a7e9496c
Lolozix <lorranasilvadamasceno@gmail.com>
2023-12-12 10:29:53 -0400

    commit

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,28 +1,23 @@
 import Card from "../Componentes/Card";
 import Menu from '../Componentes/Menu';
 import "../styles.css"
-export default function Home(){
+export default function Home() {
+  const lista = JSON.parse(localStorage.getItem("Lista")) || [];
 
-  const lista = JSON.parse(localStorage.getItem("Lista"))
-  
-  return(
-    <div>
-      
-      <Menu/>
-
-
-
-      {lista.map((cortes) => 
-
-        <Card cortes = {cortes}/>
-
-      )}
-
-
-
+  if (lista.length === 0) {
+    return (
+      <div>
+        <Menu />
       </div>
-  )
+    );
+  }
 
-  
- 
+  return (
+    <div>
+      <Menu />
+      {lista.map((cortes) => (
+        <Card cortes={cortes} key={cortes.id} />
+      ))}
+    </div>
+  );
 }
